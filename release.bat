a70c325a00f354a4711931e5064af4b5be30b128
darkconsole <darkcee.legit@gmail.com>
2022-11-03 15:32:12 -0500

    maybe this

--- release.bat
+++ release.bat
@@ -2,6 +2,10 @@
 
 call php patch.php push effoff
 call php patch.php push effon
+copy .github\README.md UT2-README.md
+copy .github\LICENSE.md UT2-LICENSE.md
 
-call git archive -o dse-untamed-v%~1.zip master *.esp UT2-README.md UT2-LICENSE.md configs fomod interface meshes patches scripts seq sound textures
+call "\Program Files\7-Zip\7z.exe" a -tzip -r -xr@.gitignore dse-untamed-v%~1.zip *.esp UT2-README.md UT2-LICENSE.md configs fomod interface meshes patches scripts seq sound textures
 
+del UT2-README.md
+del UT2-LICENSE.md
