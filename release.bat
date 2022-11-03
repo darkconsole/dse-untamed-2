9ff4e78f6e95219545478e0eff83ceb5fc82c195
darkconsole <darkcee.legit@gmail.com>
2022-11-03 15:17:18 -0500

    will github hate that i renamed readme

--- release.bat
+++ release.bat
@@ -3,5 +3,5 @@
 call php patch.php push effoff
 call php patch.php push effon
 
-call git archive -o dse-untamed-v%~1.zip master *.esp configs fomod interface meshes patches scripts seq sound textures
+call git archive -o dse-untamed-v%~1.zip master *.esp UT2-README.md UT2-LICENSE.md configs fomod interface meshes patches scripts seq sound textures
 
