3323738c8cbdd96c8df79a0f37483126b713553c
darkconsole <darkcee.legit@gmail.com>
2022-11-03 16:07:57 -0500

    started with "i can probably make the script more simple"

--- release.bat
+++ release.bat
@@ -2,10 +2,10 @@
 
 call php patch.php push effoff
 call php patch.php push effon
-copy .github\README.md UT2-README.md
-copy .github\LICENSE.md UT2-LICENSE.md
+copy .github\README.md UT2-README.md > NUL
+copy .github\LICENSE.md UT2-LICENSE.md > NUL
 
-call "\Program Files\7-Zip\7z.exe" a -tzip -r -xr@.gitignore dse-untamed-v%~1.zip *.esp UT2-README.md UT2-LICENSE.md configs fomod interface meshes patches scripts seq sound textures
+call "\Program Files\7-Zip\7z.exe" a -tzip -r -xr@.gitignore dse-untamed-v%~1.zip *.esp UT2-README.md UT2-LICENSE.md configs fomod interface meshes patches scripts seq sound textures > NUL
 
 del UT2-README.md
 del UT2-LICENSE.md
